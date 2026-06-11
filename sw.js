bff28474ec4a96697b5c91e81d4412c903888bbe
DHShin6023 <iseya80@naver.com>
2026-06-11 14:09:58 +0900

    fix: 프롬프트 재조정 - 독립 용기 기준 식별, 볶음밥 내부 재료 분리 금지

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'calorie-app-v11';
+const CACHE = 'calorie-app-v12';
 const ASSETS = ['./index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
